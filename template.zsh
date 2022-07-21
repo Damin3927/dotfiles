a84c42a9d017a03ea817601d1db33b887da9d82a
Hiroaki Honda <hddamin6844@gmail.com>
2022-07-21 16:25:15 +0900

    Add llvm env path

--- template.zsh
+++ template.zsh
@@ -8,7 +8,11 @@ autoload -Uz compinit && compinit
 # 補完で小文字でも大文字にマッチさせる
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-# PATH
+### PATH
+# zls
 export PATH="${PATH}:${HOME}/.zls"
+
+# llvm
+export PATH="${PATH}:$(brew --prefix llvm)/bin"
 
 source "$(get_abs_path $0 alias.zsh)" "$(get_abs_path $0 option.zsh)"
