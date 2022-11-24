ef04814dc9601b0b02861480ad21a70a611d4225
Hiroaki Honda <d@damin.dev>
2022-11-24 22:08:46 +0900

    Remove nvm

--- macos/zsh/alias.zsh
+++ macos/zsh/alias.zsh
@@ -26,17 +26,6 @@ if [ -e "${HOME}/.rbenv" ];then
   alias irb='unalias ruby bundle gem irb && eval "$(rbenv init -)" && irb'
 fi
 
-# lazy loading of nvm
-export NVM_DIR="$HOME/.nvm"
-. "${NVM_DIR}/nvm.sh"
-# if [ -s "${NVM_DIR}/nvm.sh" ]; then
-#   alias nvm='unalias nvm node npm yarn && . "$NVM_DIR"/nvm.sh && nvm'
-#   alias node='unalias nvm node npm yarn && . "$NVM_DIR"/nvm.sh && node'
-#   alias npm='unalias nvm node npm yarn && . "$NVM_DIR"/nvm.sh && npm'
-#   alias yarn='unalias nvm node npm yarn && . "$NVM_DIR"/nvm.sh && yarn'
-# fi
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Ruby on Rails
 alias rails='bundle exec rails'
 alias rspec='bundle exec rspec'
