7b6f2696a4822560f7603a6dd9233d0f67c43fd4
Hiroaki Honda <d@damin.dev>
2023-08-04 20:51:06 +0900

    feat: disable pyenv lazyload on poetry emulation environment

--- macos/init/pyenv/init.sh
+++ macos/init/pyenv/init.sh
@@ -7,4 +7,11 @@ _pyenv_init() {
   eval "$(pyenv init -)"
 }
 
-eval "$(lazyenv.load _pyenv_init pyenv python python3 pip poetry)"
+# if POETRY_ACTIVE=1, eval pyenv init immediately
+# otherwise, lazy load pyenv init
+if [[ "${POETRY_ACTIVE}" == "1" ]]; then
+  _pyenv_init
+  unset -f _pyenv_init
+else
+  eval "$(lazyenv.load _pyenv_init pyenv python python3 pip poetry)"
+fi
--- macos/zsh/alias.zsh
+++ macos/zsh/alias.zsh
@@ -32,6 +32,9 @@ alias pip='pip3'
 # venv
 alias venv='source .venv/bin/activate'
 
+# poetry
+alias posh='poetry shell'
+
 # NeoVim
 alias vi='nvim'
 alias vim='nvim'
