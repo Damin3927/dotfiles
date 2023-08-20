273180ef9a8f5e45a6c34da84a80d3587148a49a
Hiroaki Honda <d@damin.dev>
2023-08-21 01:13:19 +0900

    feat: Add several options

--- macos/zsh/option.zsh
+++ macos/zsh/option.zsh
@@ -7,6 +7,8 @@ setopt cdable_vars
 setopt always_to_end
 setopt auto_menu
 setopt interactive_comments
+setopt auto_param_slash
+setopt auto_param_keys
 
 # correct the spell of commands
 setopt correct
@@ -20,3 +22,14 @@ setopt list_packed
 # display completion results colorfully
 autoload colors
 zstyle ':completion:*' list-colors ''
+
+# histsize
+export HISTSIZE=1000000000
+export SAVEHIST=$HISTSIZE
+setopt extended_history
+
+# do not record commands if the user is root
+if [ $UID = 0 ]; then
+  unset HISTFILE
+  SAVEHIST=0
+fi
