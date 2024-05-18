040e8c7da935e84959afe096adea50539d763a5c
Hiroaki Honda <d@damin.dev>
2024-05-19 00:08:00 +0900

    feat: Add nvim-neotest/nvim-nio dep

--- macos/vim/config/lua/plugins.lua
+++ macos/vim/config/lua/plugins.lua
@@ -227,6 +227,7 @@ return require("packer").startup(function (use)
     'rcarriga/nvim-dap-ui',
     requires = {
       'mfussenegger/nvim-dap',
+      'nvim-neotest/nvim-nio',
     },
     config = function()
       require("dapui").setup()
