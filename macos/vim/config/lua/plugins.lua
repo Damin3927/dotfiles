43d68472cd3249052503428eb7c6f0ac92ddbb00
Hiroaki Honda <d@damin.dev>
2024-07-12 22:16:20 +0900

    feat: Add octo.nvim

--- macos/vim/config/lua/plugins.lua
+++ macos/vim/config/lua/plugins.lua
@@ -739,4 +739,21 @@ return require("packer").startup(function (use)
       vim.api.nvim_set_keymap('n', '<Leader>a', ':TestSuite<CR>', { silent = true, noremap = true })
     end
   }
+
+  -- octo
+  use {
+  'pwntester/octo.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function ()
+    require('octo').setup({
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+    })
+  end
+}
 end)
