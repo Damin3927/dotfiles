9e9d06ad09735cda068f2d84d6f627983aa7bc33
Hiroaki Honda <d@damin.dev>
2024-10-25 22:14:16 +0900

    fix: Installation of fzf

--- linux/init/fzf/install.sh
+++ linux/init/fzf/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt install -y fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}"/.fzf
+"${HOME}"/.fzf/install
--- macos/init/fzf/install.sh
+++ macos/init/fzf/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}"/.fzf
 "${HOME}"/.fzf/install
