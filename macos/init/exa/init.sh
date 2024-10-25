471431899f76a02873ebafdbecd5f26567e61889
Hiroaki Honda <d@damin.dev>
2024-10-25 20:12:45 +0900

    feat: Replace exa with eza

--- macos/init/exa/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-alias ls='exa'
-alias l='exa -1a'
-alias ll='exa -1lHh --git'
-alias la='exa -1lHha --git'
---
+++ macos/init/eza/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+alias ls='eza'
+alias l='eza -1a'
+alias ll='eza -1lHh --git'
+alias la='eza -1lHha --git'
