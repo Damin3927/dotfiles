b2ab521e448e663d41d05297a7af8a23feae2826
Hiroaki Honda <d@damin.dev>
2025-01-01 14:56:01 +0900

    feat: Stop lazy-load pyenv

--- linux/init/pyenv/init.sh
+++ linux/init/pyenv/init.sh
@@ -3,15 +3,4 @@
 export PYENV_ROOT="${HOME}/.pyenv"
 append_to_path "${PYENV_ROOT}/bin"
 
-_pyenv_init() {
-  eval "$(pyenv init -)"
-}
-
-# if POETRY_ACTIVE=1, eval pyenv init immediately
-# otherwise, lazy load pyenv init
-if [[ "${POETRY_ACTIVE}" == "1" ]]; then
-  _pyenv_init
-  unset -f _pyenv_init
-else
-  eval "$(lazyenv.load _pyenv_init pyenv python3 pip3 poetry)"
-fi
+eval "$(pyenv init -)"
