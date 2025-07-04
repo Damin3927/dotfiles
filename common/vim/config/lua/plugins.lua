6fd3cf0220584d1d1dafc084803f6a7d51400559
Hiroaki Honda <d@damin.dev>
2025-07-04 10:19:14 +0900

    feat: Remove ipkg from my treesitter install list

--- common/vim/config/lua/plugins.lua
+++ common/vim/config/lua/plugins.lua
@@ -398,7 +398,7 @@ return require("packer").startup(function (use)
         auto_install = true,
 
         -- List of parsers to ignore installing (for "all")
-        ignore_install = { "phpdoc" },
+        ignore_install = { "phpdoc", "ipkg" },
 
         ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
         -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
