4a30495ca3317de9df7750ca288a4c34a707aa71
Hiroaki Honda <d@damin.dev>
2025-07-04 10:18:30 +0900

    feat: Remove gvm

--- macos/init/goenv/init.sh
+++ macos/init/goenv/init.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+export GOENV_ROOT="$HOME/.anyenv/envs/goenv"
+prepend_to_path "${GOENV_ROOT}/bin"
+
+eval "$(goenv init -)"
+
+prepend_to_path "${GOROOT}/bin"
+append_to_path "${GOPATH}/bin"
--- macos/init/gvm/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
