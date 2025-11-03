135bc8bb5c24cddcb465ab2ca1c0a748f7fd417f
Hiroaki Honda <d@damin.dev>
2025-11-03 11:28:11 +0900

    Lazyload goenv

--- macos/init/goenv/init.sh
+++ macos/init/goenv/init.sh
@@ -3,7 +3,17 @@
 export GOENV_ROOT="$HOME/.anyenv/envs/goenv"
 prepend_to_path "${GOENV_ROOT}/bin"
 
-eval "$(goenv init -)"
+goenv() {
+  unset -f goenv
+  eval "$(command goenv init -)"
+  goenv "$@"
+}
+
+go() {
+  unset -f go
+  eval "$(command goenv init -)"
+  go "$@"
+}
 
 prepend_to_path "${GOROOT}/bin"
 append_to_path "${GOPATH}/bin"
